58c2f15a7776ccd813221b0f1fb8d31a22b7a941
Nathan <119708133+nathantr97@users.noreply.github.com>
2023-01-23 22:25:12 -0800

    added highscore button

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,9 @@
 // set variables using getElementById to create functions
 var startButton = document.getElementById('start-btn')
-
 var quizHeader = document.querySelector('h1')
-
 var quizSection = document.getElementById('quiz-box')
 var questionText = document.getElementById('question-text')
-
-
 var timer = document.getElementById('timer')
-
 var scoreEl = document.getElementById('score')
 var scoreText = document.getElementById('score-text')
 var showHighScore = document.getElementById('viewHS')
@@ -49,29 +44,30 @@ var score = 0
 var timeLeft = 100
 var currentQuestion = 0
 
+showHighScore.onclick=()=>
+console.log("High Score");
+    quizSection.classList.remove("is-hidden");
+
 
 
 function startGame() {
     //Console.log startGame to check if button works. tested in console and button works
     console.log('Game Start');
-    //function to start
-    startClock();
-    //Call loadQuestion to load the first question and its associated choices
+    startClock()
     loadQuestion();
-    //Once the first set of questions are loaded and the first answer is selected, eventListeners for the choice buttons will continue the cycle of questions.
 
 
-    //Update display of header to the first question number
+    //Update display of header after clicking start
     currentQuestion++;
     quizHeader.textContent = "YOU ONLY GOT 100 SECONDS SO MAKE IT COUNTS!"
     
-    //Displays the quiz content section by removing the 'is-hidden' class
+    //Displays the quiz content section
     quizSection.classList.remove('is-hidden');
-    //Displays the choices users can select by removing the 'is-hidden' class
+    //Displays the choices users can select 
     choiceSection.classList.remove('is-hidden');
-    //TODO: display placeholder for correct/incorrect answer by removing is-hidden class
+    // display placeholder for correct/incorrect answer
     scoreEl.classList.remove('is-hidden');
-    //TODO: hide the click to start button by adding the is-hidden class
+    // hide the click to start button
     startButton.classList.add('is-hidden');
 }
 
@@ -82,36 +78,30 @@ var gameClock = setInterval(function(){
     timer.textContent = 'Time: ' + timeLeft;
      if(timeLeft <= 0) {
         clearInterval(gameClock);
-        // endGame();
-        // displayScores();
      }
 },1000);
 }
 
-//Function that is called on startGame and will load next question, function is also called on checkFinalQuestion function
+//Function that is called on startGame and will load next question
 function loadQuestion() {
-        //Grabs the question and answers and displays question and answers
     questionText.textContent = objArray[currentQuestion].question;
     choiceOne.textContent = objArray[currentQuestion].choices[0]
     choiceTwo.textContent = objArray[currentQuestion].choices[1]
     choiceThree.textContent = objArray[currentQuestion].choices[2]
     choiceFour.textContent = objArray[currentQuestion].choices[3]
 }
-//Function that is called when when user selects an answer
+//check users selections
 function checkAnswer(event) {
-    //Check which button user selected and call function answerCompare to compare user input to the correct answer
+    let correctChoice= ["<script>","both <header> and <footer>","function=myFunction()","onclick","if (i===5)"]
     var userChoice = event.target;
     answerCompare(userChoice.textContent, objArray[currentQuestion].correctChoice);
-    //Function called to increment currentQuestion and to check if it is the final question
     checkFinalQuestion();
     console.log(score)
 }
-let correctChoice=true;
-//Function that is used to compare user selected answewr to the correctIndex in objArray 
+
+//Function that is used to compare user selections 
 function answerCompare(userChoice, correctChoice) {
     if (userChoice === correctChoice){
-        //Questions are worth 20 points if correct, if user has less than 20 points, the score will remain 0
-        //If user selected incorrect, deduct points and update text to display incorrect
             score = score + 20;
             scoreText.textContent = 'That is correct!';
         } else {
@@ -120,17 +110,13 @@ function answerCompare(userChoice, correctChoice) {
         timeLeft = timeLeft - 10;
     if (userChoice != correctChoice){
         scoreText.textContent = 'That is incorrect!';
-
     }
-        
     } 
 }
-//Function to increment currentQuestion and check if final question, if it is the last question, run the endGame function, if not call loadQuestion();
 function checkFinalQuestion(){
     currentQuestion++;
     if (currentQuestion === 5){
         return;
-        // endGame();
         // displayScores();
     }
     else {
@@ -138,44 +124,14 @@ function checkFinalQuestion(){
     }
 }
 
-// //Function is still in progress
-// function endGame() {
-//     //Hide all unnecessary areas to make space for scoreboard
-//     var quizContainer = document.getElementById('quiz-container')
-//     quizContainer.classList.remove('is-hidden');
-//     quizSection.classList.remove('is-hidden');
-//     choiceSection.classList.remove('is-hidden');
-//     scoreEl.classList.remove('is-hidden');
-//     startButton.classList.remove('is-hidden');
-    
-
     //Display submission page and scoreboard
     var submissionContainer = document.getElementById('submissionContainer')
     var submissionForm = document.getElementById('submissionForm')
-    
-    
-    
-    //TODO: stop timer if still running
-    // clearInterval(gameClock);
 
-    //TODO: display input for user Initials
-
-// }
-
-// function displayScores() {
-    //TODO: display score after submission of Initials
-    //TODO: check local storage for pre-existing
-        //if none, create new array
-    //TODO: add new score to local storage
-    //TODO: compare scores in local storage
-// }
 
 //Event listener for start button
 startButton.addEventListener('click', startGame)
-//Event listeners when user selects an answer - will checkAnswer and run through checkpoints
 choiceOne.addEventListener('click', checkAnswer)
 choiceTwo.addEventListener('click',checkAnswer)
 choiceThree.addEventListener('click', checkAnswer)
 choiceFour.addEventListener('click', checkAnswer)
-//Event listener for showing highscores, skipping quiz
-// showHighScore.addEventListener('click',displayScores)
